0ec5e1a43afe608708f221f32a1f8516dfc1d35b
Ryan McIntyre <rmcintyre598@gmail.com>
2024-01-11 13:31:23 -0500

    passed geolocation through brewery api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,8 @@
-function makeApiRequest() {
-    fetch('https://api.openbrewerydb.org/v1/breweries?by_state=florida&by_postal=32502&per_page=20')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-}
 
-const fetchButton = document.querySelector('#choice');
-
-fetchButton.addEventListener('click', async () => {
-    const name = 'micros';
-    console.log(name);
-
-    const response = await fetch(
-        `https://api.openbrewerydb.org/v1/breweries?q&per_page=20`
-    );
-
-    const data = await response.json();
-
-    // Pull out the names of the breweries
-    const breweryNames = data.map((brewery) => brewery.name);
-
-    // Log the brewery names in the console
-    console.log(breweryNames);
-});
 
 
 // loadMapScript();
-makeApiRequest();
+
 let map;
 
 // locate you.
@@ -55,8 +31,9 @@ if (navigator.geolocation) {
             let lat = parseFloat(position.coords.latitude);
             let lng = parseFloat(position.coords.longitude);
 
-//pass position to the map
+            //pass position to the map
             initMap(lat, lng)
+            makeApiRequest(lat, lng);
 
         }
 
@@ -73,4 +50,29 @@ function handleLocationError() {
     alert("Error: The Geolocation service failed.")
 
 };
+function makeApiRequest(lat, lng) {
+    fetch(`https://api.openbrewerydb.org/v1/breweries?by_dist=${lat},${lng}&per_page=8`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+}
+
+const fetchButton = document.querySelector('#choice');
+
+fetchButton.addEventListener('click', async () => {
+    const name = 'micros';
+    console.log(name);
+
+    const response = await fetch(
+        `https://api.openbrewerydb.org/v1/breweries?q&per_page=20`
+    );
+
+    const data = await response.json();
+
+    // Pull out the names of the breweries
+    const breweryNames = data.map((brewery) => brewery.name);
+
+    // Log the brewery names in the console
+    console.log(breweryNames);
+});
 
