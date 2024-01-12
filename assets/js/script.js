b6035bac4a0b87e03b113e0ea205959741984185
Rob <jbyrd126@gmail.com>
2024-01-11 20:19:20 -0600

    changed header. got map to take listed breweries and drop pins.tried to put logo in

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,12 +24,12 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
         (position) => {
 
-             lat = parseFloat(position.coords.latitude);
+            lat = parseFloat(position.coords.latitude);
             lng = parseFloat(position.coords.longitude);
 
             //pass position to the map
             initMap(lat, lng)
-            //makeApiRequest(lat, lng);
+
         }
     );
 
@@ -45,34 +45,32 @@ fetchButton.addEventListener('click', async () => {
     console.log(name);
 
     fetch(`https://api.openbrewerydb.org/v1/breweries?by_type=micro&by_dist=${lat},${lng}&per_page=8`)
-    .then(response => response.json())
-    //.then(data => console.log(data))
-    .then((data) => {
-        // Loop through the breweries data
-        data.forEach((brewery) => {
-            // Extract latitude and longitude coordinates
-            const { latitude, longitude } = brewery;
+        .then(response => response.json())
+        //.then(data => console.log(data))
+        .then((data) => {
+            // Loop through the breweries data
+            data.forEach((brewery) => {
+                // Extract latitude and longitude coordinates
+                const { latitude, longitude } = brewery;
 
-            // Create a new marker for each brewery location
-            const marker = new google.maps.Marker({
-                position: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
-                map: map, // Add the marker to the map
-                title: brewery.name, // Set the marker title
+                // Create a new marker for each brewery location
+                const marker = new google.maps.Marker({
+                    position: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
+                    map: map, // Add the marker to the map
+                    title: brewery.name, // Set the marker title
+                });
             });
+
+
+
+
+
+            // Pull out the names of the breweries
+            const breweryNames = data.map((brewery) => brewery.name);
+
+            // Log the brewery names in the console
+            console.log(breweryNames);
         });
-  
-  
-  
-    // const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_type=micro&by_dist=${lat},${lng}&per_page=8`);
-
-   // const data = await response.json();
-
-    // Pull out the names of the breweries
-    const breweryNames = data.map((brewery) => brewery.name);
-
-    // Log the brewery names in the console
-    console.log(breweryNames);
-});
 })
 
 function handleLocationError() {
@@ -82,7 +80,7 @@ function handleLocationError() {
 
 };
 function makeApiRequest(lat, lng) {
-    
+
     fetch(`https://api.openbrewerydb.org/v1/breweries?by_dist=${lat},${lng}&per_page=8`)
         .then(response => response.json())
         //.then(data => console.log(data))
@@ -91,14 +89,14 @@ function makeApiRequest(lat, lng) {
             data.forEach((brewery) => {
                 // Extract latitude and longitude coordinates
                 const { latitude, longitude } = brewery;
-               
+
                 // Create a new marker for each brewery location
                 const marker = new google.maps.Marker({
                     position: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
                     map: map, // Add the marker to the map
                     title: brewery.name, // Set the marker title
                 });
-                
+
             });
             console.log(data)
         })
@@ -110,7 +108,7 @@ function makeApiRequest(lat, lng) {
 
 
 
-}
+};
 
 const fetchChoice = document.querySelector('#choice');
 const fetchChoice1 = document.querySelector('#choice1');
@@ -192,7 +190,7 @@ fetchChoice4.addEventListener('click', async () => {
     const response = await fetch(
         `https://api.openbrewerydb.org/v1/breweries?by_type=bar`
     );
- main
+    main
 
 
         .catch(error => console.error(error));
@@ -200,3 +198,4 @@ fetchChoice4.addEventListener('click', async () => {
 
 
 });
+//makeApiRequest(lat, lng);
